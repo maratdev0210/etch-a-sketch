2bf8754dd912c92de7adb0d90820ec5551adae37
Marat Musagitov <maratforwork1@gmail.com>
2023-11-11 17:21:00 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# etch-a-sketch
+# etch-a-sketch project
+**New Features**
++ added ability to change the size of the grid
++ the grid cells change their colors whenever the mouse is being hovered over them
---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+// width and height in pixels
+const GridContainerHeight = 800;
+const GridContainerWidth = 800;
+
+let gridContainer = document.querySelector('.grid-container');
+
+
+function createGrid(size)
+{
+    for (let row = 0; row < size; ++row) {
+        let rowContainer = document.createElement('div'); // store each row divs inside one container
+        for (let col = 0; col < size; ++col) {
+            let grid = document.createElement('div');
+            grid.classList.add('grid');
+            let gridHeight = GridContainerHeight / size;
+            let gridWidth = GridContainerWidth / size; 
+            grid.style.width = `${gridWidth}px`;
+            grid.style.height = `${gridHeight}px`;
+            grid.addEventListener('mouseover', (event) => {
+                grid.style.backgroundColor = 'blue';
+            });
+            rowContainer.appendChild(grid);
+        }
+        gridContainer.appendChild(rowContainer);
+    }
+}
+
+window.addEventListener('load', (event) => {
+    createGrid(16);
+});
+
+let changeSize = document.querySelector('.change-size');
+
+changeSize.addEventListener('click', (event) => {
+    let size = parseInt(prompt('Enter the size: '));
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid(size);
+});
